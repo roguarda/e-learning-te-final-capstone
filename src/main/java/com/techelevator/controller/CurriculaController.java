03754d4573a62db9f8712b0963d6168765b3f902
Rocio Guarda <rocio.guarda@gmail.com>
2022-08-22 10:50:25 -0300

    register

--- src/main/java/com/techelevator/controller/CurriculaController.java
+++ src/main/java/com/techelevator/controller/CurriculaController.java
@@ -58,49 +58,46 @@ public class CurriculaController {
         return "redirect:/teacherHomePage?curriculaName=" + curricula.getCurriculaName();
     }
 
-    @RequestMapping(path = "/Curricula/edit/{curriculaId}", method = RequestMethod.GET)
-    public String editCurriculaForm(ModelMap modelMap, int curriculaId) {
-        Curricula curricula = curriculaDAO.getById(curriculaId);
-        modelMap.put("curricula", curricula);
+//    @RequestMapping(path = "/Curricula/edit/{curriculaId}", method = RequestMethod.GET)
+//    public String editCurriculaForm(ModelMap modelMap, int curriculaId) {
+//        Curricula curricula = curriculaDAO.getById(curriculaId);
+//        modelMap.put("curricula", curricula);
+//        return "Teacher/Curricula/editCurricula";
+
+//    }
+
+//    @RequestMapping(path = "/Curricula/edit/{curriculaId}", method = RequestMethod.POST)
+//    public String editCurriculaForm(
+//            @Valid @ModelAttribute("editCurricula") Curricula curricula,
+//            BindingResult result,
+//            RedirectAttributes flash
+//    ) {
+//        if (result.hasErrors()) {
+//            flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "editCurricula", result);
+//            return "redirect:/Teacher/Curricula/editCurricula";
+//        }
+//        flash.addFlashAttribute("message", "You have successfully edited the curricula.");
+////        curriculaDAO.update(flash.);
+//        return "redirect:/Curricula?curriculaName=" + curricula.getCurriculaName();
+//    }
+
+
+        //edit GET
+    @RequestMapping("Curricula/edit/{id}")
+    public String editCurriculaForm(@PathVariable Integer curriculaId, ModelMap map, @RequestParam String updateCriteria, String newValue)
+    {  Curricula curricula = curriculaDAO.getById(curriculaId);
+        map.put("curricula", curricula);
         return "Teacher/Curricula/editCurricula";
-
     }
 
-    @RequestMapping(path = "/Curricula/edit/{curriculaId}", method = RequestMethod.POST)
-    public String editCurriculaForm(
-            @Valid @ModelAttribute("editCurricula") Curricula curricula,
-            BindingResult result,
-            RedirectAttributes flash
-    ) {
-        if (result.hasErrors()) {
-            flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "editCurricula", result);
-            return "redirect:/Teacher/Curricula/editCurricula";
-        }
-        flash.addFlashAttribute("message", "You have successfully edited the curricula.");
-//        curriculaDAO.update(flash.);
-        return "redirect:/Curricula?curriculaName=" + curricula.getCurriculaName();
+    //edit POST
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+    public String editCurriculaForm(@PathVariable Integer curriculaId, String updateCriteria, String newValue)
+    {
+        curriculaDAO.update(updateCriteria, newValue, curriculaId);
+
+        return "redirect:/curricula";
     }
-
-
-    //    //edit GET
-//    @RequestMapping("/edit/{id}")
-//    public String editForm(@PathVariable Integer id, ModelMap map)
-//    {
-//        Genre genre = genreDao.getById(id);
-//
-//        map.put("genre", genre);
-//
-//        return "genres/edit";
-//    }
-//
-//    //edit POST
-//    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
-//    public String editSubmit(@PathVariable Integer id, @ModelAttribute Genre genre)
-//    {
-//        genreDao.update(id, genre);
-//
-//        return "redirect:/genres";
-//    }
 //
 //
 
