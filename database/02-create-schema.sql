7967692da3758f92eb036b1ed84820e01414e006
Micaela Pesado <pesado.micaela1@gmail.com>
2022-08-24 11:30:05 -0300

    delete course id from curricula y curricula_id from course

--- database/02-create-schema.sql
+++ database/02-create-schema.sql
@@ -28,13 +28,10 @@ CREATE TABLE curricula
     curricula_id      SERIAL        NOT NULL UNIQUE PRIMARY KEY,
     curricula_name    VARCHAR(55)   not null,
     daily_instruction VARCHAR(1000) NOT NULL,
-    daily_homework    VARCHAR(1000) NOT NULL,
-    course_id          SERIAL NOT   NULL,
-    FOREIGN KEY (course_id) REFERENCES course (course_id)
+    daily_homework    VARCHAR(1000) NOT NULL
 );
 
-/*si cada curso puede tener más de una curricula (pero cada currícula un sólo curso) hay que quitar curricula_id de course.
-  y la tabla course_curricula no es necesaria*/
+
 CREATE TABLE course
 (
     course_id          SERIAL       NOT NULL UNIQUE PRIMARY KEY,
@@ -43,10 +40,7 @@ CREATE TABLE course
     course_description varchar(255) NOT NULL,
     difficulty         varchar(10)  NOT NULL,
     cost               varchar(10),
-    curricula_id       INT,
-    FOREIGN KEY (teacher_id) REFERENCES app_user (user_id),
-    foreign key (curricula_id) REFERENCES curricula (curricula_id)
-
+    FOREIGN KEY (teacher_id) REFERENCES app_user (user_id)
 );
 
 CREATE TABLE curricula_course
